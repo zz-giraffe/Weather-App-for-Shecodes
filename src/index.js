23737dcb1d3e29eb41465ef102b350b67f34ddb1
zz-giraffe <150102664+zz-giraffe@users.noreply.github.com>
2024-04-26 13:07:28 +0800

    add the time

--- src/index.js
+++ src/index.js
@@ -2,13 +2,33 @@ function displayWeather(response) {
     let temperatureElement = document.querySelector("#weather-info-temp");
     let cityElement = document.querySelector("#city");
     let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let timeElement = document.querySelector("#time");
+    let date = new Date(response.data.time * 1000);
+    console.log(response.data);
 
 
     cityElement.innerHTML = response.data.city;
     temperatureElement.innerHTML = Math.round(response.data.temperature.current) + "°C";
-    descriptionElement.innerHTML = response.data.weather.description;
+    descriptionElement.innerHTML = response.data.condition.description;
+    humidityElement.innerHTML = response.data.temperature.humidity + "%";
+    timeElement.innerHTML = formatDate(date);
    
 }
+
+function formatDate(date) {
+
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    let day = days[date.getDay()];
+   
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+
+    return `${day} ${hours}:${minutes}`;
+  }
 
 
 function searchCity(city) {
